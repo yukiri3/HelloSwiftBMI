f53da20ba9ea5cca585ccb4120fe1a830140591f
user <user@Mac.local>
2017-03-09 14:28:08 +0800

    first commit

---
+++ HelloSwiftBMI/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  HelloSwiftBMI
+//
+//  Created by user on 2017/3/8.
+//  Copyright © 2017年 HeartCoding. All rights reserved.
+//
+
+import UIKit
+
+class Model: NSObject {
+
+}
