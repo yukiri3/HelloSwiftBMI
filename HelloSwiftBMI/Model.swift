6582e1868a2be3c665592555b15ddbfb7b1921a4
user <user@Mac.local>
2017-03-09 14:29:59 +0800

    update

--- HelloSwiftBMI/Model.swift
+++ HelloSwiftBMI/Model.swift
@@ -9,5 +9,24 @@
 import UIKit
 
 class Model: NSObject {
-
+    //計算BMI 
+    public func calculateBMI(height:Float,weight:Float)->Float{
+        return (weight * 10000) / (height * height);
+    }
+    
+    //計算BMI範圍
+    public func calculateBMIRange(BMIValue:Float)->String{
+        if BMIValue < 18.5 {
+             return "重量不足"
+        }
+        else if BMIValue < 24{
+             return "正常"
+        }
+        else if BMIValue < 27{
+            return "過胖"
+        }
+        else {
+            return "肥宅"
+        }
+    }
 }
--- HelloSwiftBMI/ViewController.swift
+++ HelloSwiftBMI/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet var heightTextField: UITextField!//輸入身高textField
+    @IBOutlet var weightTextField: UITextField!//輸入體重textField
+    @IBOutlet var bmiSum: UILabel!//顯示BMI結果 Label
+    @IBOutlet var bmiRangeString: UILabel!//顯示BMI結果範圍 Label
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    heightTextField.keyboardType = UIKeyboardType.decimalPad
+    weightTextField.keyboardType = UIKeyboardType.decimalPad
+        heightTextField.delegate = self
+        weightTextField.delegate = self
     }
-
+    @IBAction func submit(_ sender: UIButton) {
+     let heightFloat = (heightTextField.text! as NSString).floatValue
+     let weightFloat = (weightTextField.text! as NSString).floatValue
+     
+        //將輸入的身高體重 利用寫好的model運算
+     let sum = Model().calculateBMI(height: heightFloat,weight: weightFloat)
+        //將結果顯示出來
+        bmiSum.text = "BMI:"+String(format:"%.2f", sum)
+        bmiRangeString.text = "BMI範圍："+Model().calculateBMIRange(BMIValue: sum)
+        
+    }
+    
+    //縮鍵盤
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
 }
 
